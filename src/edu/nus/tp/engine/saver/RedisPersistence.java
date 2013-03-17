6add8b2f01c7e21e1d3bcf4308240890fe276806
priyanshujha <priyanshu.jha@gmail.com>
2013-03-17 23:51:02 +0800

    Arun's commit rolled back

--- src/edu/nus/tp/engine/saver/RedisPersistence.java
+++ src/edu/nus/tp/engine/saver/RedisPersistence.java
@@ -1,12 +1,18 @@
 package edu.nus.tp.engine.saver;
 
-import static edu.nus.tp.engine.utils.Constants.*;
+import static edu.nus.tp.engine.utils.Constants.NEGATIVE;
+import static edu.nus.tp.engine.utils.Constants.NEUTRAL;
+import static edu.nus.tp.engine.utils.Constants.PASSWORD;
+import static edu.nus.tp.engine.utils.Constants.POSITIVE;
+import static edu.nus.tp.engine.utils.Constants.REDIS_HOST;
+import static edu.nus.tp.engine.utils.Constants.REDIS_PORT;
+import static edu.nus.tp.engine.utils.Constants.TERM_COUNT_BY_CATEGORY;
+import static edu.nus.tp.engine.utils.Constants.TWEET_COUNT_BY_CATEGORY;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.Tuple;
@@ -14,131 +20,141 @@ import edu.nus.tp.engine.utils.Category;
 
 public class RedisPersistence implements Persistence {
 
-	private ConcurrentHashMap <String, AtomicLong> positiveTermMap = new ConcurrentHashMap <String, AtomicLong>();
+	/*private ConcurrentHashMap <String, AtomicLong> positiveTermMap = new ConcurrentHashMap <String, AtomicLong>();
 	private ConcurrentHashMap <String, AtomicLong> negativeTermMap = new ConcurrentHashMap <String, AtomicLong>();
-	private ConcurrentHashMap <String, AtomicLong> neutralTermMap = new ConcurrentHashMap <String, AtomicLong>();
-	
+	private ConcurrentHashMap <String, AtomicLong> neutralTermMap = new ConcurrentHashMap <String, AtomicLong>();*/
+
 	private Jedis jedis=null;
-	
+
 	public RedisPersistence() {
-		
+
 		jedis=new Jedis(REDIS_HOST, REDIS_PORT);
 		jedis.auth(PASSWORD);
+		//jedis=new Jedis("127.0.0.1");
 		jedis.connect();
 	    System.out.println("Connected");
 	}
-	
+
 	@Override
 	public void saveTermsAndClassification(Collection<String> eachParsedTweet,
 			Category category) {
 
 		//for calculation of priors
 		incrementTweetCountFor(category);
-		
+
 		for (String eachTerm : eachParsedTweet) {
-		
+
 			incrementTermCountFor(category);
-			
+
 			switch (category) {
 				case POSITIVE:
 					//TODO need to check if this is expensive
-					positiveTermMap.putIfAbsent(eachTerm, new AtomicLong(0)); //init value set at 1 for Laplace smoothing
-					positiveTermMap.get(eachTerm).incrementAndGet();
+					/*positiveTermMap.putIfAbsent(eachTerm, new AtomicLong(0)); //init value set at 1 for Laplace smoothing
+					positiveTermMap.get(eachTerm).incrementAndGet();*/
+					addPositiveTerm(eachTerm);
 					break;
-				
+
 				case NEGATIVE:
-					negativeTermMap.putIfAbsent(eachTerm, new AtomicLong(0));
-					negativeTermMap.get(eachTerm).incrementAndGet();
+					/*negativeTermMap.putIfAbsent(eachTerm, new AtomicLong(0));
+					negativeTermMap.get(eachTerm).incrementAndGet();*/
+					addNegativeTerm(eachTerm);
 					break;
-					
+
 				case NEUTRAL:
-					neutralTermMap.putIfAbsent(eachTerm, new AtomicLong(0));
-					neutralTermMap.get(eachTerm).incrementAndGet();
+					/*neutralTermMap.putIfAbsent(eachTerm, new AtomicLong(0));
+					neutralTermMap.get(eachTerm).incrementAndGet();*/
+					addNeutralTerm(eachTerm);
 					break;
-					
+
 				default:
 					break;
 				}
-			
+
 		}
-		
+
 		System.out.println("Done");
-	
+
 	}
-	
+
 
 	public long getUniqueTermsInVocabulary(){
-		
+
 		Set<String> uniqueTermsInVocabulary=new HashSet<String>();
-		uniqueTermsInVocabulary.addAll(positiveTermMap.keySet());
-		uniqueTermsInVocabulary.addAll(negativeTermMap.keySet());
-		uniqueTermsInVocabulary.addAll(neutralTermMap.keySet());
-		
+		uniqueTermsInVocabulary.addAll(getTermsInClass(POSITIVE).keySet());
+		uniqueTermsInVocabulary.addAll(getTermsInClass(NEGATIVE).keySet());
+		uniqueTermsInVocabulary.addAll(getTermsInClass(NEUTRAL).keySet());
+
 		return uniqueTermsInVocabulary.size();
-		
+
 	}
-	
-	
-	
+
+
+
 	/**
 	 * Gets prior for a category
 	 * @param category
 	 * @return
 	 */
 	public double getPriorClassificationForCategory(Category category){
-		
+
 		long totalInstances=0,thisCategoryCount=0;
-		
+
 		Set<Tuple> tweetCountByCategory = jedis.zrangeWithScores(TWEET_COUNT_BY_CATEGORY, 0, -1);
-		
+
 		for (Tuple eachCategory : tweetCountByCategory) {
 			totalInstances+=eachCategory.getScore();
 		}
-	
+
 		Double thisCategoryCountDbl=jedis.zscore(TWEET_COUNT_BY_CATEGORY, category.toString());
-		
+
 		thisCategoryCount=thisCategoryCountDbl==null?1:thisCategoryCountDbl.longValue();
-		
+
 		return (double)thisCategoryCount/totalInstances;
-		
-	}
-	
-	
-	public long getTotalUniquePositiveTerms(){
-		return positiveTermMap.size();
-	}
-	
-	public long getTotalUniqueNegativeTerms(){
-		return negativeTermMap.size();
-	}
-	
-	public long getTotalUniqueNeutralTerms(){
-		return neutralTermMap.size();
+
 	}
 
-	
+
+	public long getTotalUniquePositiveTerms(){
+		return getTermsInClass(POSITIVE).keySet().size();
+	}
+
+	public long getTotalUniqueNegativeTerms(){
+		return getTermsInClass(NEGATIVE).keySet().size();
+	}
+
+	public long getTotalUniqueNeutralTerms(){
+		return getTermsInClass(NEUTRAL).keySet().size();
+	}
+
+
 	public long getTermFrequencyInCategory(String term, Category category){
-		
+
 		switch (category) {
-		
-		case POSITIVE: return positiveTermMap.get(term)==null?1:positiveTermMap.get(term).longValue();
-			
-		case NEGATIVE:  return negativeTermMap.get(term)==null?1:negativeTermMap.get(term).longValue();
-			
-		case NEUTRAL:  return neutralTermMap.get(term)==null?1:neutralTermMap.get(term).longValue();
-			
+
+		case POSITIVE: return getTermFrequencyInClass(POSITIVE,term);
+
+		case NEGATIVE:  return getTermFrequencyInClass(NEGATIVE,term);
+
+		case NEUTRAL:  return getTermFrequencyInClass(NEUTRAL,term);
+
 		default: return 0;
 		}
-		
+
+	}
+
+	private long getTermFrequencyInClass(String className, String term) {
+		String termFrequencyString=jedis.hget(className, term);
+		return termFrequencyString==null?1:Long.parseLong(termFrequencyString);
+		//positiveTermMap.get(term)==null?1:positiveTermMap.get(term).longValue();
 	}
 
 	public long getTermCountByCategory(Category category) {
 		Double thisCategoryCountDbl=jedis.zscore(TERM_COUNT_BY_CATEGORY, category.toString());
 		return thisCategoryCountDbl==null?1:thisCategoryCountDbl.longValue();
-		
+
 	}
-	
-	
+
+
 	private void incrementTermCountFor(Category category) {
 		jedis.zincrby(TERM_COUNT_BY_CATEGORY, 1, category.toString());
 	}
@@ -146,9 +162,29 @@ public class RedisPersistence implements Persistence {
 
 	private void incrementTweetCountFor(Category category) {
 		jedis.zincrby(TWEET_COUNT_BY_CATEGORY, 1, category.toString());
-		
+
 	}
-	
+
+	private void addPositiveTerm(String term) {
+		addTermToClass(POSITIVE, term);
+	}
+
+	private void addNegativeTerm(String term) {
+		addTermToClass(NEGATIVE, term);
+	}
+
+	private void addNeutralTerm(String term) {
+		addTermToClass(NEUTRAL, term);
+	}
+
+	private void addTermToClass(String key, String term) {
+		jedis.hincrBy(key, term,1);
+	}
+
+	private Map<String,String> getTermsInClass(String className) {
+		return jedis.hgetAll(className);
+	}
+
 	//No lifecycle method here 
 	//FIXME Didn't disconnect any connections here 
 
--- src/edu/nus/tp/engine/utils/Constants.java
+++ src/edu/nus/tp/engine/utils/Constants.java
@@ -5,20 +5,24 @@ import java.util.Set;
 import com.google.common.collect.Sets;
 
 public class Constants {
-	
+
 	public static final String EMPTY_STRING="";
 	public static final String SPACE=" ";
-	
+
 	public static final String TWEET_COUNT_BY_CATEGORY="TWEET_COUNT_BY_CATEGORY";
 	public static final String TERM_COUNT_BY_CATEGORY="TERM_COUNT_BY_CATEGORY";
-	
+
 	public static final String REDIS_HOST = "50.30.35.9";
 	public static final int REDIS_PORT = 2871;
 	public static final String PASSWORD = "5cb88a1bcb1c2baae9335c4f964caf9d";
-	
+
+	public static final String POSITIVE = "POSITIVE";
+	public static final String NEUTRAL = "NEUTRAL";
+	public static final String NEGATIVE = "NEGATIVE";
+
 	public static final Set<String> STOP_WORDS=Sets.newHashSet("I","a","about","an","are","as","at","be","by","com","for","from","how","in","is","it","of","on","or","that","the","this","to","was","what","when","where","who","will","with","the","www");
-	
-	
+
+
 /**STOP WORDS FROM GOOGLE HISTORY **/
 /**********************************
 http://www.ranks.nl/resources/stopwords.html
@@ -57,5 +61,5 @@ the
 www
 
 */
-	
+
 }
